9524ee614683aeb1171f9d0048a8bbd9f0f27493
Iqbal Fasyah <balfasyah@gmail.com>
2021-08-25 23:56:13 +0700

    Add Layout Admin Dashboard

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -1,5 +1,6 @@
 <?php
 
+
 namespace App\Providers;
 
 use Illuminate\Cache\RateLimiting\Limit;
@@ -26,7 +27,7 @@ class RouteServiceProvider extends ServiceProvider
      *
      * @var string|null
      */
-    // protected $namespace = 'App\\Http\\Controllers';
+    protected $namespace = 'App\\Http\\Controllers';
 
     /**
      * Define your route model bindings, pattern filters, etc.
@@ -42,6 +43,10 @@ class RouteServiceProvider extends ServiceProvider
                 ->middleware('api')
                 ->namespace($this->namespace)
                 ->group(base_path('routes/api.php'));
+                
+                Route::prefix('admin')
+                ->namespace('App\Http\Controllers\Admin')
+                ->group(base_path('routes/web.php'));
 
             Route::middleware('web')
                 ->namespace($this->namespace)
